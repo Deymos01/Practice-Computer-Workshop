9e7b55924910d5bafe747c798e7b2da08a10cf18
Deymos01 <bananofruslan@gmail.com>
2022-10-07 22:30:42 +0300

    Updated 3 files

--- Homework/Homework-2/file1.cpp
+++ Homework/Homework-2/file1.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
 
+int main() {
+    int n;
+    do {
+        std::cout << "Input N (N >= 0):";
+        std::cin >> n;
+    } while (n < 0);
+
+    for (int i = 1; i <= n; i++){
+        for(int j = 1; j<=i; j++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
