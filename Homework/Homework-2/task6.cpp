5e230c91e5d739fcf5960f3e8cb57ff20f0e839a
Deymos01 <bananofruslan@gmail.com>
2022-10-08 00:47:09 +0300

    Added file

---
+++ Homework/Homework-2/task6.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+int main() {
+    int n;
+    do {
+        std::cout << "Input N (N >= 0 and N <= 25):";
+        std::cin >> n;
+    } while (n < 0 || n > 25);
+
+    int quantity_stars = -1;
+    for (int i = 0; i < 2 * n - 1; i++) {
+        //Изменение количества выводимых звездочек
+        //Если не дошли до середины ромба, то увеличиваем количество звездочек, иначе - уменьшаем
+        (i < (2 * n - 1) / 2 + 1) ? quantity_stars += 2 : quantity_stars -= 2;
+
+        //Печать пробелов перед звездочками
+        for (int k = 0; k < abs(2 * (n - i - 1)); k++) {
+            std::cout << " ";
+        }
+
+        //Вывод звездочек
+        for (int j = 0; j < quantity_stars; j++) {
+            if (j == 0 || j == quantity_stars - 1){
+                std::cout << "* ";
+            } else {
+                std::cout << "  ";
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
