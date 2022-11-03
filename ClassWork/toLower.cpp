985f693f2f3fbf70da5f1fa7b90c86e578e50910
Deymos01 <bananofruslan@gmail.com>
2022-11-03 17:58:17 +0300

    Added file

---
+++ ClassWork/toLower.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void printStr(char str[]){
+	int i = 0;
+	while (str[i] != '\0'){
+		std::cout << str[i];
+		++i;
+	}
+	std::cout << std::endl;
+}
+
+void toLower(char str[]){
+	while (*str){
+		if ('A' <= str[0] && str[0] <= 'Z')
+			str[0] += 32; 
+		++str;
+	}
+}
+
+int main()
+{
+	char str[] = "Lorem IpsUm";
+	std::cout << "Initial string: ";
+	printStr(str);
+	toLower(str);
+	std::cout << "Lowercase string: ";
+	printStr(str);
+	return 0;
+}
