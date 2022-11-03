6e7353ef2d97aae72313f97ab1c3844cff214156
Deymos <91058963+Deymos01@users.noreply.github.com>
2022-11-03 15:00:19 +0300

    Update reverseArray.cpp

--- ClassWork/reverseArray.cpp
+++ ClassWork/reverseArray.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
-void reverseArray(int* arr, size_t n){
-	for (size_t indx = 0; indx < n; ++indx){
-		std::cout << arr[n - indx - 1] << " ";
+void fillArray(int* arr, int n){
+	for (int i = 0; i < n; ++i){
+		arr[i] = i+1;
+	}
+}
+
+void printArray(int* arr, int n){
+	for (int i = 0; i < n; ++i){
+		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
+}
+
+void reverseArray(int* arr, size_t n){
+	for (size_t indx = 0; indx < n/2; ++indx){
+		int temp = arr[indx];
+		arr[indx] = arr[n - indx - 1];
+		arr[n - indx - 1] = temp;
+	}
 }
 
 int main(){
@@ -12,12 +26,13 @@ int main(){
 	std::cout << "Input size of array: ";
 	std::cin >> sizeArray;	
 	int *arr = new int(sizeArray);
-	std::cout << "Input " << sizeArray << " items:" << std::endl;
-	for (int i = 0; i < sizeArray; ++i){
-		std::cin >> arr[i];
-	}
-
+	fillArray(arr, sizeArray);
+	std::cout << "\ninitial array: \n";
+	printArray(arr, sizeArray);
 	reverseArray(arr, sizeArray);
+	std::cout << "\nreverse array: \n";
+	printArray(arr, sizeArray);
+	delete[] arr; 
 
 	return 0;
 }
